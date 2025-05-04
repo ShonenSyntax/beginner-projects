00d3e3008e17ffff11266ef7eecb80df074e1544
Pratyaksh <reach.pratyaksh@gmail.com>
2025-05-04 15:56:33 +0530

    Add madlibs.py script

---
+++ madlibs.py
@@ -0,0 +1,8 @@
+adj = input('Adjective: ')
+verb1 = input('Verb: ')
+verb2 = input('Verb: ')
+hero = input('Favorite Hero: ')
+
+madlib = f'Python programming is just so {adj}. Man, I want to {verb1} or maybe {verb2}. \
+By the way, do you like {hero}?'
+print(madlib)

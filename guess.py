576ad28d1ef508a5f0714f55255c0f6e05f506d0
Pratyaksh <reach.pratyaksh@gmail.com>
2025-05-06 13:00:29 +0530

    Add another guess game where the computer guesses our number

--- guess.py
+++ guess.py
@@ -22,3 +22,25 @@ guess(3)
 #----------------Note------------------------
 #random.randint(x, y) → includes both x and y.
 #range(x, y) → includes x, but excludes y.
+
+def comp_guess(x):
+    low = 1
+    high = x
+    comp_num= 0
+    feedback = ''
+
+    while feedback != 'c':
+        if low != high:
+            comp_num = random.randint(low,high)
+        elif low == high:
+            comp_num= low
+        feedback = input(f'Is {comp_num} too high(H) or too low(L) or Correct(C)?: ').lower()
+        if feedback == 'h':
+            high = comp_num-1
+        elif feedback == 'l':
+            low = comp_num+1
+    
+    print(f'Congratulations! The computer guess your number,{comp_num} correctly.')
+
+comp_guess(100)
+        
